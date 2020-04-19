f33458be78a2dcc649d08043b135df198d0442a8
cornelisser <retrodevelopment@gmail.com>
2020-04-19 11:51:06 +0200

    TTFM/SMS FIX Porto Slide
    Fixed the porto slide for FM to use note link when cmd3 is used. Tone wrap is still causing issues

--- code/replayerFM.asm
+++ code/replayerFM.asm
@@ -225,8 +225,8 @@ replay_decodedata:
 	;xor	a
 	;ld	(AY_regMIXER),a	;set mixer to silence
 	;--- Set the tone table base
-	ld	hl,CHIP_ToneTable
-	ld	(replay_Tonetable),hl
+;	ld	hl,CHIP_ToneTable
+;	ld	(replay_Tonetable),hl
 
 
 	
@@ -241,8 +241,8 @@ replay_decodedata:
 	and	a
 	jp	nz,_rdd_3psg
 	
-	ld	hl,CHIP_FM_ToneTable
-	ld	(replay_Tonetable),hl
+;	ld	hl,CHIP_FM_ToneTable
+;	ld	(replay_Tonetable),hl
 
 	
 _rdd_3psg:	
@@ -255,8 +255,8 @@ _rdd_3psg:
 	and	a
 	jp	z,_rdd_2psg
 	
-	ld	hl,CHIP_FM_ToneTable
-	ld	(replay_Tonetable),hl
+;	ld	hl,CHIP_FM_ToneTable
+;	ld	(replay_Tonetable),hl
 	
 	
 _rdd_2psg:		
@@ -1289,9 +1289,21 @@ _CHIPcmd3_portTone:
 		
 _CHIPcmd3_retrig:
 	;--- Check if we have a	note on the	same event
-	bit	0,(ix+CHIP_Flags)
+	ld	h,(ix+CHIP_Flags)
+	bit	0,h
 	ret	z
 
+	;-- determine which table to use
+	bit 	7,h
+	jp	nz,_cmd3_fm
+_cmd3_psg:
+	ld	hl,CHIP_ToneTable	;TRACK_ToneTable
+	jp	99f
+_cmd3_fm:
+	ld	hl,CHIP_FM_ToneTable	;TRACK_ToneTable
+	set	4,(ix+CHIP_Flags)		; FM notelink bit
+
+99:
 	;-- get the	previous note freq
 	ld	a,(replay_previous_note)
 	add	a
